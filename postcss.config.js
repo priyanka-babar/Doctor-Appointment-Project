f92b3d43af3cd960cfc9e96e143fec55b52e9a15
Priyanka <babarpriyanka620@gmail.com>
2025-11-03 12:57:58 +0530

    chnage for new branch

--- postcss.config.js
+++ postcss.config.js
@@ -5,3 +5,13 @@ export default {
   plugins: [tailwind(), autoprefixer()],
 };
 
+// import tailwind from "@tailwindcss/postcss";
+// import autoprefixer from "autoprefixer";
+
+// export default {
+  // plugins: [tailwind(), autoprefixer()],
+// };
+
+
+
+
