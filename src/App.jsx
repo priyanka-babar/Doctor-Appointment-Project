6d060009aba78157cbcfecb845e13752d67885cb
Priyanka <babarpriyanka620@gmail.com>
2025-11-03 11:55:57 +0530

    Added local frontend project files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./index.css"; // ✅ Make sure Tailwind is imported here instead of App.css
+
+function App() {
+
+  return (
+    <>
+
+      <div className="text-green-600 text-3xl font-bold text-center mt-10">
+        Tailwind is Working ✅
+      </div>    </>
+
+  );
+}
+
+export default App;
