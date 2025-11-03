0eee1e4b9ce31b116106125dbfee9b4c14ec179c
Priyanka <babarpriyanka620@gmail.com>
2025-11-03 12:33:03 +0530

    new change

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+import tailwind from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [tailwind(), autoprefixer()],
 };
+
